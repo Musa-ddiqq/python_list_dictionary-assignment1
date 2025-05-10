94acd7fd3cb4214203e06e217fcf0e03ace66215
Musaddiq Qureshi <musaddiqqureshii@gmail.com>
2025-05-10 23:34:04 +0500

    Add files via upload

---
+++ list_dictionary.py
@@ -0,0 +1,14 @@
+# Creating list named fruits 
+fruits = ["apple", "banana", "mango"]
+
+# entire list of fruits 
+print("Fruits list:", fruits)
+
+# second item in the list
+print("Second fruit:", fruits[1])
+
+#  Adding new fruit to the list using .append()
+fruits.append("orange")
+
+# updated list
+print("Updated fruits list:", fruits)
