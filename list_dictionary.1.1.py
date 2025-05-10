d554b4802ea972ebbcb9206303afaea251e80242
Musaddiq Qureshi <musaddiqqureshii@gmail.com>
2025-05-10 23:40:30 +0500

    Add files via upload

---
+++ list_dictionary.1.1.py
@@ -0,0 +1,15 @@
+# Creating dictionar
+student = {
+    "name": "Musa",
+    "age": 18,
+    "grade": "A"
+}
+
+print("Student Info:", student)
+print("Name:", student["name"])
+
+# 4. Adding new key-value pair
+student["school"] = "GPGC"
+
+# 5. Printing updated dictionary
+print("Updated Student Info:", student)
